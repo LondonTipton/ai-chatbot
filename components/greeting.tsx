11d2b19dda2af2441291b1e4b60232a269160b6f
LondonTipton <80815215+LondonTipton@users.noreply.github.com>
2025-10-08 06:12:27 +0200

    local database
    fixed the local database issue

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -8,7 +8,7 @@ export const Greeting = () => {
     >
       <motion.div
         animate={{ opacity: 1, y: 0 }}
-        className="font-semibold text-xl md:text-2xl"
+        className="font-semibold text-3xl md:text-5xl"
         exit={{ opacity: 0, y: 10 }}
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.5 }}
@@ -21,7 +21,7 @@ export const Greeting = () => {
       </motion.div>
       <motion.div
         animate={{ opacity: 1, y: 0 }}
-        className="text-xl text-zinc-500 md:text-2xl"
+        className="text-2xl text-zinc-500 md:text-3xl"
         exit={{ opacity: 0, y: 10 }}
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.6 }}
